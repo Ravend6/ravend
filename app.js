da530d250f6600052539033e3b51a2f5445f02f8
Ravend6 <ravend6@gmail.com>
2015-03-05 16:44:56 +0200

    Add mumbo

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var port = Number(process.env.PORT) || 3000;
 
 app.use(express.static("public"));
 
+app.get("/portfolio/mumbo", function (req, res) {
+  res.sendFile(__dirname + "/public/portfolio/mumbo/index.html");
+});
+
 app.get("/portfolio/sport-trail", function (req, res) {
   res.sendFile(__dirname + "/public/portfolio/sport-trail/index.html");
 });
