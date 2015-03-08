679edc3dfca7e46107a7e558e65922b16a82bc29
Ravend6 <ravend6@gmail.com>
2015-03-08 15:19:59 +0200

    Add nunjucks and home page

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var nunjucks = require('nunjucks');
+
+var nun = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+nun.express(app);
+
 
 var port = Number(process.env.PORT) || 3000;
 
+
 app.use(express.static("public"));
+
+app.get('/', function (req, res) {
+  res.render('pages/index.html', {
+    name: process.env.NAME
+  });
+});
 
 app.get("/portfolio/mumbo", function (req, res) {
   res.sendFile(__dirname + "/public/portfolio/mumbo/index.html");
